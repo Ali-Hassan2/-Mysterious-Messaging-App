4002606f78b690ffd01f7a2e1d0623ddce440958
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-26 19:07:40 +0500

    working on zod schemas

--- src/schemas/accepting.schema.ts
+++ src/schemas/accepting.schema.ts
@@ -1,1 +1,7 @@
-import 
+import { z } from "zod";
+
+const acceptingValidation = z.boolean();
+const acceptingSchema = z.object({
+  isacceptingmessages: acceptingValidation,
+});
+export { acceptingSchema };
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,2 +1,4 @@
 export { SignupSchema } from "./signUp.schema";
 export { verifySchema } from "./verify.schema";
+export { signinSchema } from "./signIn.schema";
+export { acceptingSchema } from "./accepting.schema";
