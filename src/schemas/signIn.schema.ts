92a3db5cfd2f785ed0d2b519110cb9b69e3d8fe4
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-26 19:06:02 +0500

    working on zod schemas

--- src/schemas/signIn.schema.ts
+++ src/schemas/signIn.schema.ts
@@ -1,2 +1,11 @@
 import { z } from "zod";
 
+const identifierValidation = z.string().email({ message: "Invalid email" });
+const passwordValidation = z.string();
+
+const signinSchema = z.object({
+  identifier: identifierValidation,
+  password: passwordValidation,
+});
+
+export { signinSchema };
