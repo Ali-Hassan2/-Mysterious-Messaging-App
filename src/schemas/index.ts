80667b3e7a565bbb75aa5003cc544e91df14972d
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-26 19:10:26 +0500

    working on zod schemas

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,3 +2,4 @@ export { SignupSchema } from "./signUp.schema";
 export { verifySchema } from "./verify.schema";
 export { signinSchema } from "./signIn.schema";
 export { acceptingSchema } from "./accepting.schema";
+export { messageSchema } from "./message.schema";
--- src/schemas/message.schema.ts
+++ src/schemas/message.schema.ts
@@ -1,1 +1,17 @@
-import 
+import { z } from "zod";
+
+const contentValidation = z
+  .string()
+  .min(1, { message: "Message should atleast have one character." })
+  .max(500, { message: "Message should can have 500 characters." });
+const createdatValidation = z
+  .date()
+  .optional()
+  .default(() => new Date());
+
+const messageSchema = z.object({
+  content: contentValidation,
+  createdat: createdatValidation,
+});
+
+export { messageSchema };
