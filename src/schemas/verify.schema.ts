279f8600378ad6b52b2b6b04a6d4f769fc5eeaee
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-26 19:03:31 +0500

    working on zod schemas

--- src/schemas/verify.schema.ts
+++ src/schemas/verify.schema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 const verifycodeValidation = z
   .string()
-  .minLength(6, { message: "Code should b 6 chars long." })
-  .max(6, { message: "Code should be 6 chars max" });
+  .min(6, { message: "Code should be exactly 6 characters long." })
+  .max(6, { message: "Code should be exactly 6 characters long." });
 
 const verifySchema = z.object({
   verifycode: verifycodeValidation,
