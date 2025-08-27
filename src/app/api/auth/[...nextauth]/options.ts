c0005bc41f2bb1ab748ef477a915cf50162d0a91
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-27 17:53:31 +0500

    signup route done

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,42 @@
+import { NextAuthOptions } from "next-auth";
+import Credentials, {
+  CredentialsProvider,
+} from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { UserModel } from "@/model";
+import { connect_db } from "@/lib";
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "text", placeholder: "Your email..." },
+        password: {
+          label: "password",
+          type: "password",
+          placeholder: "Your password",
+        },
+        async authorize(credentials: any): Promise<any> {
+          await connect_db();
+          try {
+            const user = await UserModel.findOne({
+              $or: [
+                { email: credentials.identifier },
+                { username: credentials.identifier },
+              ],
+            });
+            if (!user) {
+              throw new Error("No user found with this email");
+            }
+            if (!user.isVerified) {
+              throw new Error("Please verfiy you account first.");
+            }
+            const isCorrectPassword = await bcr
+          } catch (error) {}
+        },
+      },
+    }),
+  ],
+};
