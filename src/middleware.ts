549537807a37c50dfc4147041d37895bec9e61dd
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-30 17:51:10 +0500

    route sign in adding

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,13 +4,14 @@ import { getToken } from "next-auth/jwt";
 async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
-  if(token && (
-    url.pathname.startsWith('/signin') ||
-    url.pathname.startsWith('/signup') || 
-    url.pathname.startsWith('/verify') || 
-    url.pathname.startsWith('/')
-  )){
-    return NextResponse.redirect(new URL)
+  if (
+    token &&
+    (url.pathname.startsWith("/signin") ||
+      url.pathname.startsWith("/signup") ||
+      url.pathname.startsWith("/verify") ||
+      url.pathname.startsWith("/"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard"));
   }
 }
 const config = {
