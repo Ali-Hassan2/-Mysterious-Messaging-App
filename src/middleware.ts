cd7f1ebfcbf94ae8c64e3befef7f42b01dbedf10
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-30 17:47:16 +0500

    route sign in adding

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
 import { NextResponse, NextRequest } from "next/server";
 export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
-async function middleware(request: NextRequest) {}
+async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+  if(token)
+}
 const config = {
   matcher: ["/signin", "/signup", "/"],
 };
