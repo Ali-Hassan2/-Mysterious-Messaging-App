0e94f84828af27156208cd4a555d5a75922163cd
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-30 17:48:32 +0500

    route sign in adding

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,7 @@ async function middleware(request: NextRequest) {
   if(token && (
     url.pathname.startsWith('/signin') ||
     url.pathname.startsWith('/signup') || 
+    url.pathname
   ))
 }
 const config = {
