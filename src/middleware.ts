7afd2f5ca67dcfcc495b101fe2b040952c74b705
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-30 17:52:43 +0500

    route sign in adding

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,7 +14,7 @@ async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  return NextResponse.redirect
+  return NextResponse.redirect(new URL)
 }
 const config = {
   matcher: ["/signin", "/signup", "/"],
