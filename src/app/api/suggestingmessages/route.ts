17aacfe123023e108dee005d2e15f4bd5619a2e1
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-31 21:36:35 +0500

    Integarating AI

---
+++ src/app/api/suggestingmessages/route.ts
@@ -0,0 +1,12 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+
+export const runtime = "edge";
+
+export async function POST(request:Request){
+    
+}
