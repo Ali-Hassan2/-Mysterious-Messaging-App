310c8429c0ae4b1e1b30f532d0e51598f5c0aee9
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-31 17:53:20 +0500

    working on accepting-messages-toggle-functionallity

--- src/app/api/accpetingmessages/route.ts
+++ src/app/api/accpetingmessages/route.ts
@@ -32,5 +32,41 @@ export async function POST(request: Request) {
         { status: 404 }
       );
     }
-  } catch (error) {}
+    return Response.json({
+      success: true,
+      message: "User accepting Messgaes Flag updated.",
+      updatedUserWithFlag,
+    });
+  } catch (error) {
+    console.log("There is an error", error);
+    return Response.json({
+      success: false,
+      message: "Internal Server error",
+      error: error?.message,
+    });
+  }
+}
+
+export async function GET(request: Request) {
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user;
+  if (!session || !session?.user) {
+    return Response.json({
+      success: false,
+      message: "User not Authenticated",
+    });
+  }
+  const userId = user._id;
+  const UserExist = await UserModel.findById(userId);
+  if (!UserExist) {
+    return Response.json({
+      success: false,
+      message: "User Not Found",
+    });
+  }
+  return Response.json({
+    success: false,
+    message: "User Found successfully",
+    isAcceptingMessages: UserExist.isAcceptingMessages,
+  });
 }
