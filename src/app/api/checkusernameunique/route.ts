82ede655b0ab4bd814604cd68322b2d4bb35aa62
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-31 14:33:40 +0500

    updated

--- src/app/api/checkusernameunique/route.ts
+++ src/app/api/checkusernameunique/route.ts
@@ -9,6 +9,27 @@ const UsernameQuerySchema = z.object({
 
 async function GET(request: Request) {
   await connect_db();
+  const { searchParams } = new URL(request.url);
+  const queryParams = {
+    username: searchParams.get("username"),
+  };
+  const result = UsernameQuerySchema.safeParse(queryParams);
+  console.log("The result is:", result);
+  if (!result.success) {
+    const usernameErrors = result.error.format().username?._errors || [];
+    return Response.json(
+      {
+        success: false,
+        message:
+          usernameErrors?.length > 0
+            ? usernameErrors?.join(", ")
+            : "Invalid query params",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
   try {
   } catch (error) {
     console.log("Error while checking username", error);
