d943a8155119391c943eb13841c4d9a6f1d98a04
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-09-02 11:27:25 +0500

    Integarating AI

--- src/app/api/suggestingmessages/route.ts
+++ src/app/api/suggestingmessages/route.ts
@@ -11,7 +11,7 @@ export const runtime = "edge";
 export async function POST(request: Request) {
   try {
     const prompt =
-      "Create a list of three open-ended and engaging questions formatted as a single string. Each questions should be separated by '||'.These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audiance. Avoid personal or sensitive topics, focusing instead on univesal themes that encourage friendly interaction. For example your output should be structured like this: 'What's a hobby you've recently started? || If you could have dinner with any historical figure, who would it be? || What's simple thing that makes you happy'";
+      "Create a list of three open-ended and engaging questions formatted as a single string. Each questions should be separated by '||'.These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audiance. Avoid personal or sensitive topics, focusing instead on univesal themes that encourage friendly interaction. For example your output should be structured like this: 'What's a hobby you've recently started? || If you could have dinner with any historical figure, who would it be? || What's simple thing that makes you happy?' Ensure the questions are intriguing, foster curiosuty and contribute to a positive and welcoming conversational enviornment.";
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       stream: true,
