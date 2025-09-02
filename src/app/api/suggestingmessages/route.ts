f5182c278ddd7b0ce99e989d3d55603332402de3
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-09-02 11:24:54 +0500

    Integarating AI

--- src/app/api/suggestingmessages/route.ts
+++ src/app/api/suggestingmessages/route.ts
@@ -10,7 +10,7 @@ export const runtime = "edge";
 
 export async function POST(request: Request) {
   try {
-    const prompt = "Create a list of three open-ended"
+    const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each questions should be separated by '||'.These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audiance. Avoid personal or sensitive topics, focusing instead on univesal themes that encourage friendly interaction. " 
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       stream: true,
