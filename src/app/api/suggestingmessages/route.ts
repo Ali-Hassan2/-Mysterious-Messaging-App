9b498e1b066b294489e268146faa4c938e583860
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-09-02 11:35:40 +0500

    Integarating AI

--- src/app/api/suggestingmessages/route.ts
+++ src/app/api/suggestingmessages/route.ts
@@ -1,5 +1,4 @@
-import OpenAI, { APIError } from "openai";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import { model } from "mongoose";
 import { NextResponse } from "next/server";
 
 const openai = new OpenAI({
@@ -12,12 +11,21 @@ export async function POST(request: Request) {
   try {
     const prompt =
       "Create a list of three open-ended and engaging questions formatted as a single string. Each questions should be separated by '||'.These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audiance. Avoid personal or sensitive topics, focusing instead on univesal themes that encourage friendly interaction. For example your output should be structured like this: 'What's a hobby you've recently started? || If you could have dinner with any historical figure, who would it be? || What's simple thing that makes you happy?' Ensure the questions are intriguing, foster curiosuty and contribute to a positive and welcoming conversational enviornment.";
-    const response = await openai.completions.create({
-      model: "gpt-3.5-turbo-instruct",
-      max_tokens: 200,
-      stream: true,
-      prompt,
+    const response = await fetch("https://api.deepseek.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.AI_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "deepseek-chat",
+        prompt,
+      }),
     });
+    if (!response.ok) {
+      const error_Data = await response.json().catch(() => null);
+      throw new Error(error_Data?.message || "Cannot process AI");
+    }
     const stream = OpenAIStream(response);
     return new StreamingTextResponse(stream);
   } catch (error) {
@@ -32,7 +40,6 @@ export async function POST(request: Request) {
         },
         { status }
       );
-      
     } else {
       console.error("There is an Unexpected error", error);
       throw error;
