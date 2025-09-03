d1d8b73e37b89cec9b0d66fa5acd5d297300828a
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-09-03 21:45:08 +0500

    addming Auth.js SignIn

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,1 +1,34 @@
-"use client"
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { signinSchema } from "@/schemas";
+import { ApiResponse } from "@/types";
+import { signIn } from "next-auth/react";
+
+const page = () => {
+  const [signingIn, setSigningIn] = useState<boolean>(false);
+  const router = useRouter();
+  const form = useForm<z.infer<typeof signinSchema>>({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {
+      redirect: false,
+      identifier: "",
+      password: "",
+    },
+  });
+  const onSubmit = async (data: z.infer<typeof signinSchema>) => {
+    setSigningIn(true);
+    const response = await signIn("credentials", {
+      identifier: data.identifier,
+      password: data.password,
+    });
+  };
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
