e891349e5feb10baca2a0d4d594395f8a00f979c
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-09-04 11:02:06 +0500

    Auth Completed

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,10 +1,9 @@
-import Navbar from "@/components/manual/navbar"
 import React from "react"
 
 const page = () => {
   return (
     <div>
-      <Navbar />
+    
     </div>
   )
 }
--- src/components/manual/navbar.tsx
+++ src/components/manual/navbar.tsx
@@ -1,14 +1,29 @@
+"use client"
 import { useSession, signOut } from "next-auth/react"
 import { User } from "next-auth"
 import Link from "next/link"
+import { Button } from "../ui/button"
 const Navbar = () => {
   const { data: session } = useSession()
   const user: User = session?.user as User
   return (
     <>
-      <nav className="flex justify-between items-center p-8">
+      <nav className="flex justify-between items-center p-8 shadow-b-2xl">
         <div className="">Anonymous App</div>
-        <div></div>
+        <div>
+          {session ? (
+            <>
+              <p>Welcome, {user?.username || user.email}</p>
+              <Button onClick={() => signOut()}>Logout</Button>
+            </>
+          ) : (
+            <>
+              <Link href="/signin">
+                <Button>Login</Button>
+              </Link>
+            </>
+          )}
+        </div>
       </nav>
     </>
   )
