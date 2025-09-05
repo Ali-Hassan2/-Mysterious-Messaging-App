fbad8e290c36975da861564edf26bf6a93195933
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-09-05 17:15:14 +0500

    user dashboard under progess

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -3,7 +3,7 @@ import React from "react"
 const page = () => {
   return (
     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quam illo alias 
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quam illo 
     </div>
   )
 }
