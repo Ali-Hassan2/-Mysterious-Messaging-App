c5d1ae2e9886f176ff5a63de8da24c91fa140977
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-09-06 16:08:41 +0500

    Working-on-messages-cards

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -12,9 +12,10 @@ const page = () => {
             Your Messages
           </div>
           <div className="right w-[50vw] h-full flex justify-center items-center">
-            <div className="box h-[10vw] w-[20vw] bg-blue-200 p-2">
+            <div className="box h-[4vw] w-[20vw] bg-blue-200 p-2 flex items-center justify-betweer">
               Your Messages
             </div>
+            
           </div>
         </div>
       </div>
