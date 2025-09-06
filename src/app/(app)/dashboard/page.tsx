c7dcb61ee215bb90c72d9877e47d50c58cdcd053
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-09-06 15:40:45 +0500

    Working-on-messages-cards

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -3,9 +3,19 @@
 const page = () => {
   return (
     <>
-      <div className="w-full h-full border-4 border-red-600">
-        <div className="upper h-[100px] border-4 border-blue-600">
+      <div className="w-full h-[90vh] border-4 border-red-600">
+        <div className="upper h-[100px] border-4 border-blue-600 text-[2vw] font-bold pl-3">
           User Dashboard
+        </div>
+        <div className="h-[300px] border-2 border-green-400 flex items-center justify-center">
+          <div className="left w-[50vw] border-4 border-red-700 h-full">
+            Your Messages
+          </div>
+          <div className="right w-[50vw] h-full flex justify-center items-center">
+            <div className="box h-[2vw] w-[20vw] bg-blue-200">
+              Your Messages
+            </div>
+          </div>
         </div>
       </div>
     </>
