ba30013ce87e3129d4c128be74de66f83b361a8b
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-09-08 16:15:25 +0500

    dashboard in progess

--- src/app/api/getmessages/route.ts
+++ src/app/api/getmessages/route.ts
@@ -1,56 +1,45 @@
-import { getServerSession } from "next-auth";
-import { connect_db } from "@/lib";
-import { UserModel } from "@/model";
-import { authOptions } from "../auth/[...nextauth]/options";
-import { User } from "next-auth";
-import mongoose from "mongoose";
+import { connect_db } from "@/lib"
+import { UserModel } from "@/model"
+import mongoose from "mongoose"
 
 export async function GET(request: Request) {
-  const session = await getServerSession();
-  const user: User = session?.user as User;
-  if (!user) {
-    return Response.json(
-      {
-        success: false,
-        message: "User Not Authenticated.",
-      },
-      {
-        status: 401,
-      }
-    );
-  }
+  await connect_db()
 
-  const userId = new mongoose.Types.ObjectId(user._id);
-  // TODO: will learn about mongodb aggregations
+  // 🚨 TEMPORARY HARDCODED USER ID for testing
+  const userId = new mongoose.Types.ObjectId("68b8477fdaf9d9aceed25633")
+
   try {
-    const user = await UserModel.aggregate([
-      { $match: { id: userId } },
+    const userAgg = await UserModel.aggregate([
+      { $match: { _id: userId } },
       { $unwind: "$messages" },
       { $sort: { "messages.createdAt": -1 } },
       { $group: { _id: "$_id", messages: { $push: "$messages" } } },
-    ]);
-    if (!user) {
+    ])
+
+    if (!userAgg || userAgg.length === 0) {
       return Response.json(
         {
           success: false,
-          messages: "User not found",
+          message: "User not found or no messages",
         },
-        {
-          status: 404,
-        }
-      );
+        { status: 404 }
+      )
     }
+
     return Response.json({
       success: true,
-      message: "Messages reterived successfully",
-      messages: user[0].messages,
-    });
-  } catch (error) {
-    console.log("Ther is an error", error);
-    return Response.json({
-      success: false,
-      message: "Internal Server Error",
-      error: error?.message,
-    });
+      message: "Messages retrieved successfully",
+      messages: userAgg[0].messages,
+    })
+  } catch (error: any) {
+    console.log("There is an error", error)
+    return Response.json(
+      {
+        success: false,
+        message: "Internal Server Error",
+        error: error?.message,
+      },
+      { status: 500 }
+    )
   }
 }
