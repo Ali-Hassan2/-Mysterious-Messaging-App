07a75bfa3a1d864305806ed74403abdf227233ea
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-09-12 14:04:37 +0500

    comments-removed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,5 +4,3 @@ import { authOptions } from "./options";
 const handler = NextAuth(authOptions);
 
 export {handler as GET , handler as POST}
-
-// its going worst ever bro please help in getting it over
