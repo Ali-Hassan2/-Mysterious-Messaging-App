e22b90fed982a5051d3d63cf874610d854ed593a
Ali Hassan <alihassan26032004@gmail.com>
2025-09-14 20:57:57 +0400

    Remember me Functionality added

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,67 +1,82 @@
-"use client";
+"use client"
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { signinSchema } from "@/schemas";
-import { signIn, SignInResponse } from "next-auth/react";
+import { useState, useEffect } from "react"
+import { useRouter } from "next/navigation"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { signinSchema } from "@/schemas"
+import { signIn, SignInResponse } from "next-auth/react"
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormLabel,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Loader2 } from "lucide-react";
-import Link from "next/link";
-import { showToast } from "@/Utils";
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
+import Link from "next/link"
+import { showToast } from "@/Utils"
 
 const Page = () => {
-  const [signingIn, setSigningIn] = useState(false);
-  const router = useRouter();
+  const [signingIn, setSigningIn] = useState(false)
+  const router = useRouter()
 
   const form = useForm<z.infer<typeof signinSchema>>({
     resolver: zodResolver(signinSchema),
     defaultValues: {
       identifier: "",
       password: "",
+      remeberMe: false,
     },
-  });
+  })
+
+  useEffect(() => {
+    const savedPerson = localStorage.getItem("remebered")
+
+    if (savedPerson) {
+      form.setValue("identifier", savedPerson)
+      form.setValue("remeberMe", false)
+    }
+  }, [form])
 
   const onSubmit = async (data: z.infer<typeof signinSchema>) => {
     try {
-      setSigningIn(true);
-
+      setSigningIn(true)
+      if (data.remeberMe) {
+        localStorage.setItem("remembered", data.identifier)
+      } else {
+        localStorage.removeItem("remebered")
+      }
       const response: SignInResponse | undefined = await signIn("credentials", {
         redirect: false,
         identifier: data.identifier,
         password: data.password,
-      });
-      console.log("The response is:", response);
+      })
+      console.log("The response is:", response)
+
       if (response?.ok) {
-        showToast("Login Successfull!", "success");
+        showToast("Login Successfull!", "success")
         setTimeout(() => {
-          router.replace("/dashboard");
-        }, 3000);
+          router.replace("/dashboard")
+        }, 3000)
       } else {
-        console.error("Login failed:", response);
-        showToast("Login Failed.", "error");
-        setSigningIn(false);
+        console.error("Login failed bhai:", response?.error)
+        showToast("Login Failed.", "error")
+        setSigningIn(false)
       }
     } catch (error: any) {
       if (error instanceof Error) {
-        showToast(error?.message || "Something went Wrong. Try Again later.");
+        showToast(error?.message || "Something went Wrong. Try Again later.")
       } else {
-        showToast("An Unexpected Error occured.");
+        showToast("An Unexpected Error occured.")
       }
     } finally {
-      setSigningIn(false);
+      setSigningIn(false)
     }
-  };
+  }
 
   return (
     <div className="flex justify-center items-center min-h-screen shadow-2xl">
@@ -112,6 +127,27 @@ const Page = () => {
               )}
             />
 
+            <FormField
+              name="remeberMe"
+              control={form.control}
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <div className="border-1 flex gap-[10px]">
+                      <input
+                        type="checkbox"
+                        id="remeberMe"
+                        checked={field.value}
+                        onChange={(e) => field.onChange(e.target.checked)}
+                        disabled={signingIn}
+                      />
+                      <FormLabel htmlFor="remeberMe">Remeber Me</FormLabel>
+                    </div>
+                  </FormItem>
+                )
+              }}
+            />
+
             <Button type="submit" disabled={signingIn} className="w-full">
               {signingIn ? (
                 <>
@@ -136,7 +172,7 @@ const Page = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
