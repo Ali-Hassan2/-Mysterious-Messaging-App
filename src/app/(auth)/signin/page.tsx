5b1bc0cce0232cb8054e24c2168fff01c6716b95
Ali Hassan <alihassan26032004@gmail.com>
2025-09-16 12:52:29 +0400

    ...

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -67,7 +67,7 @@ const Page = () => {
         showToast("Login Failed.", "error")
         setSigningIn(false)
       }
-      // TODO: NEXT JOB
+      // TODO: NEXT
     } catch (error: any) {
       if (error instanceof Error) {
         showToast(error?.message || "Something went Wrong. Try Again later.")
