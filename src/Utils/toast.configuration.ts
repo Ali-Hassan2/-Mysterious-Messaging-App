74e08956120ef629f0d2c96e8bedc911da230ed1
Ali Hassan <alihassan26032004@gmail.com>
2025-09-22 17:34:25 +0500

    removed

--- src/Utils/toast.configuration.ts
+++ src/Utils/toast.configuration.ts
@@ -22,6 +22,7 @@ const showToast = (
     fontSize: "14px",
     borderRadius: "15px",
   }
+  
   switch (type) {
     case "success":
       toast.success(message, { ...defaultOptions, style: customStyle })
